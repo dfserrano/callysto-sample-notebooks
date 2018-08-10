6217675d37e944b4a85c35856656845c7ef87739
ChloeColeongco <chloecoleongco@gmail.com>
2018-08-10 16:23:09 -0600

    edits

--- notebooks/HIPModules/Tests/pythonTest.py
+++
@@ -1,9 +0,0 @@
-
-import  plotly.plotlyplotly.p  as py
-import plotly.figure_factory as ff
-import pandas as pd
-
-df = pd.read_csv("testChart.csv")
-
-table = ff.create_table(df)
-py.iplot(table, filename='tableTest')
